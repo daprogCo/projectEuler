9424896c7fa82a8cd1e8b50ca212b4e38629b2ed
daprog.co <daprog.co@gmail.com>
2023-10-16 16:24:48 -0400

    Renaming files with 3-digits numbers

--- JavaScriptSolutions/3_largestPrimeFactor.js
+++
@@ -1,6 +0,0 @@
-const largestPrimeFactor = num => {
-    for (let i = 2; i * i <= num; i++) {
-        while (num % i === 0 && num != i) num /= i;
-    }
-    return num;
-}
--- PythonSolutions/1_multiplesOf3and5.py
+++
@@ -1,6 +0,0 @@
-def multiplesOf3and5(num):
-    sum = 0
-    for i in range(1, num + 1):
-        if i % 3 == 0 or i % 5 == 0:
-            sum = sum + i
-    return sum
