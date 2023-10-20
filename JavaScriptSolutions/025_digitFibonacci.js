4e64c8eb1f818fad8d523567f8275d7ad330f742
daprog.co <daprog.co@gmail.com>
2023-10-20 00:27:21 -0400

    add solution 25 js

---
+++ JavaScriptSolutions/025_digitFibonacci.js
@@ -0,0 +1,14 @@
+const digitFibonacci = n => {
+    let val1 = BigInt(1);
+    let val2 = BigInt(1);
+    let i = 2;
+    while(true) {
+        i++;
+        let val = val1 + val2;
+        if (val.toString().length >= n) break;
+        val1 = val2;
+        val2 = val
+    }
+    return i;
+}
+ 
