4a36fbb69f4cfe03c9411e07c304998010cfb1e6
daprog.co <daprog.co@gmail.com>
2025-03-05 19:36:40 -0500

    modifs 001

--- PythonSolutions/001_multiplesOf3and5.py
+++ PythonSolutions/001_multiplesOf3and5.py
@@ -1,6 +1,17 @@
+# My answer
 def multiplesOf3and5(num):
     sum = 0
-    for i in range(1, num + 1):
+    for i in range(3, num):
         if i % 3 == 0 or i % 5 == 0:
             sum = sum + i
     return sum
+
+# Solution
+def solution(n: int = 1000) -> int:
+    return sum(e for e in range(3, n) if e % 3 == 0 or e % 5 == 0)
+
+if __name__ == '__main__':
+    num = 1000
+    print(multiplesOf3and5(num))
+    print(solution())
+ 
